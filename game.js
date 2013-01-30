4bbca93d917427a8978753064174e6592eb249b9
Alejandro Saucedo <axsauze@gmail.com>
2013-01-30 17:12:01 +0000

    ADD checkIfExists(answer)

--- game.js
+++ game.js
@@ -7,13 +7,23 @@ var nextwordboard = document.getElementById("nextword").value;
 
 function checkAnswer() {
 	var answer = nextwordboard;
+	if (answer != '')
+		checkIfExists(answer);
+
 	return false;
 }
 
 
 
-function checkIfExists() {
-	var json = $.getJSON("a.json", function(data) {
-		console.log("yes")
+function checkIfExists(answer) {
+	var exists = false;
+	console.log("inside");
+	var json = $.getJSON("letters/a.json", function(data) {
+		for(var i = 0; i < data.length; i++)
+			if (data[i] === $.trim(answer))
+				return true;
+
+		return false;
 	});
+	console.log(json);
 }
