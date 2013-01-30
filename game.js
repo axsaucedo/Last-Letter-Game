6f8048dd3e433efbdc1302bb34b1c01d534b480e
Rob Finean <rfinean@iee.org>
2013-01-30 15:07:05 +0000

    correction to nextboardword

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
 var scoreboard = document.getElementById("score").innerHTML;
 var lastwordboard = document.getElementById("lastword").innerHTML;
 var countdownboard = document.getElementById("timer").innerHTML;
-var nextwordboard = document.getElementById("nextword").innerHTML;
+var nextwordboard = document.getElementById("nextword").value;
