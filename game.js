0f0dee8197f11f6023fccacac3006e38b18d28a8
Rob Finean <rfinean@iee.org>
2013-01-30 15:17:19 +0000

    sync with Alex mid-flow

--- game.js
+++ game.js
@@ -2,3 +2,11 @@ var scoreboard = document.getElementById("score").innerHTML;
 var lastwordboard = document.getElementById("lastword").innerHTML;
 var countdownboard = document.getElementById("timer").innerHTML;
 var nextwordboard = document.getElementById("nextword").value;
+
+
+
+function checkAnswer() {
+	var answer = nextwordboard;
+	return false;
+}
+
