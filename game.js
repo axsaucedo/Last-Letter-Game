480dbb564426a65ccf8bfc3bbaff203a811d59db
Rob Finean <rfinean@iee.org>
2013-01-31 15:04:41 +0000

    Game logic

--- game.js
+++ game.js
@@ -1,15 +1,37 @@
-var scoreboard = document.getElementById("score").innerHTML;
+var scoreboard = document.getElementById("score");
 var lastwordboard = document.getElementById("lastword");
 var countdownboard = document.getElementById("timer").innerHTML;
 var nextwordboard = document.getElementById("nextword");
+var lastword = "Last";
+var sentence = "";
+var score = 0;
+var timer = 10;
 
+function reset() {
+	lastwordboard.innerHTML = lastword;
+	sentence += " " + lastword;
+	console.log(sentence);
+	nextwordboard.value = lastword.substr(lastword.length, lastword.length);
+	timer = 10;
+	countdownboard.innerHTML = String(timer);
+}
 
+function pointsscored () {
+	return timer;
+}
+
+function youlose(){
+
+}
 
 function checkAnswer() {
 	var answer = nextwordboard.value;
 	if (answer != ''
 		&& checkIfExists(answer) ) {
-		lastwordboard.innerHTML = answer;
+		lastword = answer;
+		score += pointsscored();
+		scoreboard.innerHTML = String(score);
+		reset();
 	}
 
 	return false;
@@ -30,3 +52,5 @@ function checkIfExists(answer) {
 	console.log(json);
 	return true;
 }
+
+reset();
