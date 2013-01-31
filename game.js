ce45e7d073d3e9d5c01ca4f17734a5c0c92ca98d
Rob Finean <rfinean@iee.org>
2013-01-31 15:27:35 +0000

    countdown timer implemented

--- game.js
+++ game.js
@@ -1,27 +1,39 @@
 var scoreboard = document.getElementById("score");
 var lastwordboard = document.getElementById("lastword");
-var countdownboard = document.getElementById("timer").innerHTML;
+var countdownboard = document.getElementById("timer");
 var nextwordboard = document.getElementById("nextword");
 var lastword = "Last";
 var sentence = "";
 var score = 0;
 var timer = 10;
+var timeout = null;
+
+function youlose(){
+
+}
+
+function countdown() {
+	timer--;
+	countdownboard.innerHTML = String(timer);
+	if (timer == 0)
+		youlose();
+	else
+		timeout = setTimeout('countdown()', 1000);
+}
 
 function reset() {
+	clearTimeout(timeout);
 	lastwordboard.innerHTML = lastword;
 	sentence += " " + lastword;
 	console.log(sentence);
 	nextwordboard.value = lastword.substr(lastword.length - 1, lastword.length);
 	timer = 10;
 	countdownboard.innerHTML = String(timer);
+	timeout = setTimeout('countdown()', 1000);
 }
 
 function pointsscored () {
 	return timer;
-}
-
-function youlose(){
-
 }
 
 function checkAnswer() {
